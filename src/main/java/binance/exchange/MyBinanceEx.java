519b4e50c5c8713ee3591a3ba36943a3991f7763
Kongcheng1400 <kongcheng1400@icloud.com>
2021-07-21 16:45:24 +0800

    Initial commit

---
+++ src/main/java/binance/exchange/MyBinanceEx.java
@@ -0,0 +1,10 @@
+package binance.exchange;
+
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.binance.BinanceExchange;
+
+public class MyBinanceEx {
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BinanceExchange.class);
+}
